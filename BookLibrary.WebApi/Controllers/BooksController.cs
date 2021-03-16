d2db191bdcbfff43bd5212271a4e026a9a7c2970
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2021-03-16 17:19:39 +0100

    initial commit

---
+++ BookLibrary.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookLibrary.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibrary.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookservice;
+        public BooksController(IBookService bookService)
+        {
+            _bookservice = bookService;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(_bookservice.GetBooks());
+        }
+    }
+}
