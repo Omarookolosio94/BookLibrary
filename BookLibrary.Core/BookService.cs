30dd1dfba44016e1d1dfad3953087e5a4bf5bef9
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2021-03-16 17:19:39 +0100

    initial commit

---
+++ BookLibrary.Core/BookService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLibrary.Core
+{
+    public class BookService : IBookService
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book
+                {
+                    Name = "Book One",
+                    Author = "Dev Developer",
+                    Category = "Web Design"
+                }
+            };
+        }
+    }
+}
---
+++ BookLibrary.Core/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookLibrary.Core
+{
+    public interface IBookService
+    {
+        List<Book> GetBooks();
+
+    }
+}
