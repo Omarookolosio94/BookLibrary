a758c06604b6bd2ab48a1ae4cd5aaaebd7878082
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2021-03-17 16:05:26 +0100

    crud operation set up

---
+++ BookLibrary.Core/IDbClient.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookLibrary.Core
+{
+    public interface IDbClient
+    {
+        IMongoCollection<Book> GetBooksCollection();
+    }
+}
